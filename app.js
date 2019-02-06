75f3e556ee36f709e0963cc4a4d2d359851d1113
Salman-Shaik <dukesalman5@gmail.com>
2019-02-06 13:24:16 +0530

    Create A NodeJS app to communicate with psql

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const Pool = require('pg').Pool;
+
+const app = express();
+module.exports = app;
+
+const pg = new Pool({
+  database: 'postgres',
+  user: 'salmans',
+  password: '',
+  port: 5432,
+})
+const filterResults = resultArray => {
+  return resultArray.map(item => {
+    return item.number;
+  });
+}
+pg.query("CREATE TABLE IF NOT EXISTS numbers(number integer)", (err, res) => {
+  if (err) {
+    console.log(err)
+    return;
+  }
+  console.log('Table Created')
+});
+
+app.use(express.json());
+
+app.use(({ url, method},res,next) => {
+  console.log(`${method} ${url}`);
+  next();
+});
+
+app.post("/number", (req, res) => {
+  const number = req.body.number;
+  pg.query(`INSERT INTO numbers(number) VALUES (${number})`, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    }
+    res.send("Data Succesfully Inserted");
+  })
+});
+
+app.get("/number", (req, res) => {
+  pg.query("SELECT * FROM numbers", (err, result) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    }
+    res.send(filterResults(result.rows));
+  })
+});
+
+app.delete("/number", (req, res) => {
+  const number = req.body.number;
+  pg.query(`DELETE FROM numbers WHERE number=${number}`, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    }
+    res.send("Data Succesfully Deleted")
+  })
+});
